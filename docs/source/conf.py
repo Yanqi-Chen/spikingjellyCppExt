feb67765430c7bcc6264d8ac64e93cb702386caa
cyq-thinkbook <750342559@qq.com>
2020-11-13 18:01:40 +0800

    Change theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -59,7 +59,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 show_authors = True
 
