a84ca7b19646b4cc232efb9a2967a15db3ab9644
fangwei123456 <fangwei123456@pku.edu.cn>
2020-12-07 17:14:02 +0800

    增加PLIF的反向传播，未完成

--- neuron/neuron_backward.cpp
+++ neuron/neuron_backward.cpp
@@ -37,6 +37,44 @@ std::vector<at::Tensor> LIF_hard_reset_backward(
     return {grad_x, grad_v};
 }
 
+void PLIF_hard_reset_backward_cuda(
+  float* grad_x, float* grad_v, float* grad_tau,
+  const float* grad_spike, const float* grad_v_next,
+  const float* v, const float* h, const float* spike, 
+  const float & v_th, const float & v_reset, const int & size, const int & gpu_id, 
+  const float & alpha, const bool & detach_reset, const int & grad_surrogate_function_index, 
+  const float & tau);
+
+
+std::vector<at::Tensor> PLIF_hard_reset_backward(
+    torch::Tensor & grad_spike, torch::Tensor & grad_v_next,
+    torch::Tensor & v, torch::Tensor & h, torch::Tensor & spike,
+    const float & v_th, const float & v_reset, 
+    const float & alpha, const bool & detach_reset, const int & grad_surrogate_function_index,
+    const float & tau)
+{
+    CHECK_TENSOR(grad_spike);
+    CHECK_TENSOR(grad_v_next);
+    CHECK_TENSOR(v);
+    CHECK_TENSOR(h);
+    CHECK_TENSOR(spike);
+    auto grad_x = torch::zeros_like(h.data());
+    auto grad_v = torch::zeros_like(h.data());
+    auto grad_tau = torch::zeros({1}).to(h);
+    CHECK_TENSOR(grad_x);
+    CHECK_TENSOR(grad_v);
+    CHECK_TENSOR(grad_tau);
+
+    PLIF_hard_reset_backward_cuda(
+        grad_x.data_ptr<float>(), grad_v.data_ptr<float>(), grad_tau.data_ptr<float>(), 
+        grad_spike.data_ptr<float>(), grad_v_next.data_ptr<float>(),
+        v.data_ptr<float>(), h.data_ptr<float>(), spike.data_ptr<float>(),
+        v_th, v_reset, h.numel(), h.get_device(),
+        alpha, detach_reset, grad_surrogate_function_index,
+        tau);
+    return {grad_x, grad_v};
+}
+
 //bptt---------------------------------
 void LIF_hard_reset_bptt_cuda(
   float* grad_x_seq, float* grad_v, 
